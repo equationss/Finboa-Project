487d279a2b71327bf5e2c45c3b259ce2f92958b7
junaid raja <junaidraja2@hotmail.com>
2023-11-16 23:42:44 +0500

    New Update

---
+++ Finboa  Project/OtpProcessor.cs
@@ -0,0 +1,34 @@
+﻿using MailKit.Net.Imap;
+using MailKit.Search;
+using MimeKit;
+using System;
+using System.Linq;
+
+public class OtpProcessor
+{
+    public string FetchOTPFromEmail(string host, int port, string email, string password)
+    {
+        using (var client = new ImapClient())
+        {
+            client.Connect(host, port, true);
+            client.Authenticate(email, password);
+
+            var inbox = client.Inbox;
+            inbox.Open(MailKit.FolderAccess.ReadOnly);
+
+            var query = SearchQuery.NotSeen;
+            var uids = inbox.Search(query);
+
+            if (!uids.Any())
+            {
+                Console.WriteLine("No new messages.");
+                return null;
+            }
+
+            var message = inbox.GetMessage(uids.First());
+            var otp = message.TextBody;
+
+            return otp;
+        }
+    }
+}
--- Finboa  Project/Test_Methods/ValidLogin.cs
+++ Finboa  Project/Test_Methods/ValidLogin.cs
@@ -1,10 +1,14 @@
 ﻿using Finboa__Project.Test_Objects;
+using MailKit.Net.Imap;
+using MailKit.Search;
+using MimeKit;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Finboa__Project.Test_Methods
 {
@@ -26,8 +30,30 @@ namespace Finboa__Project.Test_Methods
             string password = TestContext.DataRow["password"].ToString();
             Driver("FireFox");
             Object.login(username, password);
-            Close();
+            //Close();
 
+            // Retrieve OTP from Gmail
+            string host = "imap.gmail.com";
+            int port = 993;
+            string email = "abdul@finboa.com";
+            string gmailPassword = "$Pak35tan$2023";
+
+            var otpProcessor = new OtpProcessor();
+            var otp = otpProcessor.FetchOTPFromEmail(host, port, email, gmailPassword);
+
+            // Print or use the OTP as needed
+            if (!string.IsNullOrEmpty(otp))
+            {
+                Console.WriteLine($"OTP: {otp}");
+            }
+            else
+            {
+                Console.WriteLine("No OTP found.");
+            }
+
+            Thread.Sleep(10000);
+            // Close the browser
+            Close();
         }
     }
 }
